4abfcd82f2d9de62ae041b1c49fd99c6568c44fe
Agus Priyatin <aguspriyatin80@gmail.com>
2021-07-22 15:50:32 +0700

    adding router /sendmail

--- index.js
+++ index.js
@@ -1,43 +1,53 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const app = express();
+const router = express.Router();
 require("dotenv").config();
 
-// Gmail account info
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD
-    }
-});
 
-// Email info
-// const mailOptions = {
-//     from: 'aguspriyatin80@gmail.com',
-//     to: 'sihayati@gmail.com',
-//     subject: 'How to send emails using NodeJS',
-//     text: 'Follow the instructions and you will be fine'
-// };
-// jika menggunakan lampiran
-const mailOptions = {
-    from: 'aguspriyatin80@gmail.com',
-    to: 'sihayati@gmail.com',
-    // cc: 'lexus@gmail.com',
-    // bcc: 'sugar@gmail.com',
-    subject: 'How to send emails using NodeJS with ATTACHMENT',
-    text: 'Follow the instructions and you will be fine',
-    attachments: [{
-    filename: "logo.png", path: "./img/logo.png"}]
-};
-// Send email 📧  and retrieve server response
-transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
-});
+// Gmail account info
+router.get('/',(req,res)=>{
+    res.send("Home");
+})
+router.get('/sendmail',(req,res)=>{
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    });
+    // Email info
+    // const mailOptions = {
+    //     from: 'aguspriyatin80@gmail.com',
+    //     to: 'sihayati@gmail.com',
+    //     subject: 'How to send emails using NodeJS',
+    //     text: 'Follow the instructions and you will be fine'
+    // };
+    // jika menggunakan lampiran
+    const mailOptions = {
+        from: 'aguspriyatin80@gmail.com',
+        to: 'sihayati@gmail.com',
+        // cc: 'lexus@gmail.com',
+        // bcc: 'sugar@gmail.com',
+        subject: 'How to send emails using NodeJS with ATTACHMENT',
+        text: 'Follow the instructions and you will be fine',
+        attachments: [{
+        filename: "logo.png", path: "./img/logo.png"}]
+    };
+    // Send email 📧  and retrieve server response
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            // console.log('Email sent: ' + info.response);
+            res.send("email sent");
+        }
+    });
+    
+})
+
+app.use(router);
 
 const port = 3001;
 app.listen(port, () => {
